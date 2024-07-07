0a118035403756266165856357e07d6db5d3fb41
logic <logic@logics-MacBook-Pro.local>
2024-07-07 23:20:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+
+
+let the_title = "hello Elzero " ,the_stat ="Elzero web cshool" , the_time="12/25"
+
+let card =`
+    <div class="card" >
+        <h3> ${the_title}</h3>
+        <p>  ${the_stat}</p>
+        <span>  ${the_time}</span>
+    </div>
+`      
+
+document.write( ` ${card}`);
+
+
+let a = -2.3;
+let b = 25;
+let c = 30;
+let d = 20;
+
+console.log(Math.min(Math.trunc(a ,b ,c,d)));
+
+
+let thename = "  ahmed  ";
+
+console.log(thename.trim())
+
